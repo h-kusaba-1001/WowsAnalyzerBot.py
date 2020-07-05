7cfdfbab40210be883ffc34717a7ca52f867bf9a
h-kusaba-1001 <jojo1889jojo@gmail.com>
2020-07-05 15:00:40 +0900

    ひとまずプレイヤーの戦績を返すところをコミット

--- cogs/PlayerCog.py
+++ cogs/PlayerCog.py
@@ -1,4 +1,12 @@
 from discord.ext import commands
+import requests
+from pprint import pprint
+from math import ceil
+import datetime
+
+# IMPORT FROM OTHER .py FILES
+import auth
+import config
 
 # コグとして用いるクラスを定義。
 class PlayerCog(commands.Cog):
@@ -8,8 +16,71 @@ class PlayerCog(commands.Cog):
 
     # コマンドの作成。コマンドはcommandデコレータで必ず修飾する。
     @commands.command(aliases=['p'])
-    async def player(self, ctx):
-        await ctx.send('pong!')
+    async def player(self, ctx, player_name: str):
+
+        players_get_parameters = {
+            'application_id': auth.WARGAMING_APP_TOKEN,
+            'search': player_name,
+            'limit': 1
+        }
+
+        res = requests.get(config.PLAYERS_WOWS_API_URL, params=players_get_parameters)
+
+        jsonData = res.json()
+
+        if(not jsonData['data']):
+            # アカウント名がないので返す
+            await ctx.send('ないです')
+            pprint(jsonData['data'])
+            return;
+        else:
+            account_id = jsonData['data'][0]['account_id']
+
+        personal_data_get_parameters = {
+            'application_id': auth.WARGAMING_APP_TOKEN,
+            'account_id': account_id
+        }
+
+        res = requests.get(config.PERSONAL_DATA_WOWS_API_URL, params=personal_data_get_parameters)
+
+        jsonData = res.json()
+
+        data = jsonData['data'][str(account_id)]
+
+        if(data['hidden_profile'] is True):
+            await ctx.send('戦績が非公開のユーザです')
+            return;
+
+        stats_updated_at = datetime.datetime.fromtimestamp(data['stats_updated_at'], datetime.timezone(datetime.timedelta(hours=config.TIMEZONE_HOURS)))
+        nickname = data['nickname']
+
+        pvp_data = data['statistics']['pvp']
+
+        pvp_battles = pvp_data['battles']
+        pvp_wins = pvp_data['wins']
+        pvp_survived_battles = pvp_data['survived_battles']
+        pvp_damage_dealt = pvp_data['damage_dealt']
+
+        win_rate = round(pvp_wins * 100 / pvp_battles, 2)
+        survived_rate = round(pvp_survived_battles * 100 / pvp_battles, 2)
+        damage_rate = ceil(pvp_damage_dealt / pvp_battles)
+
+        comment = '\
+        プレイヤー名： {nickname} \n\
+戦闘数： {pvp_battles} \n\
+勝率： {win_rate:.2f} \n\
+平均与ダメ： {damage_rate:d} \n\
+生存率： {survived_rate:.2f} \n\
+戦績更新日時： {stats_updated_at} \n\
+※PVPの戦績のみを表示しています。'.format(
+            nickname=nickname,
+            pvp_battles=pvp_battles,
+            win_rate=win_rate,
+            damage_rate=damage_rate,
+            survived_rate=survived_rate,
+            stats_updated_at=stats_updated_at
+        )
+        await ctx.send(comment)
 
 def setup(bot):
     bot.add_cog(PlayerCog(bot))
